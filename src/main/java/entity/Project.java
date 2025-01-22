079d86323d7459e7fdf65dfae76ecde8e3d426ca
unnimaya5801 <unnimayas5801@gmail.com>
2025-01-22 17:58:51 +0530

    updated backend

---
+++ src/main/java/entity/Project.java
@@ -0,0 +1,47 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String link;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ src/main/java/respository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+import com.example.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
